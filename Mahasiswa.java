0230e4b533e2aee072b675ddf0d29f7d950024a9
Amira Taliya <amira.taliya@ui.ac.id>
2018-09-07 17:44:45 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,21 @@
+
+public class Mahasiswa {
+	private String nama;
+	private int npm;	
+	
+	public String getNama() {
+		return nama;
+	}
+	
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	
+	public int getNpm() {
+		return npm;
+	}
+	
+	public void setNpm(String npm) {
+		this.npm = npm;
+	}
+}
